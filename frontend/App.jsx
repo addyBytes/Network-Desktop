be87572e8cbbd1ae9070ba7af4113ab6197dff77
Aditya Kapoor <141937170+addyBytes@users.noreply.github.com>
2025-12-18 16:29:34 +0530

    initial commit-(Placeholder Component)

---
+++ frontend/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import "./App.css";
+import PlaceHolder from "./components/PlaceHolder";
+
+function App() {
+  const [email, setEmail] = useState("");
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-[#FFFDF7]">
+      <div className="w-[348px]">
+        <PlaceHolder
+          label="Email"
+          name="email"
+          type="email"
+          placeholder="Enter Your Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        
+      </div>
+    </div>
+    
+  );
+}
+
+export default App;
